45d70b4d88926c76d7c897befb1ee4e8f72b712c
Belen Enemark <belenenemark@gmail.com>
2019-08-31 16:20:55 -0300

    terminado ejercicio5

--- tp1/pruebaColor.js
+++ tp1/pruebaColor.js
@@ -1,29 +1,32 @@
 "use strict";
+//variables
 let ctx= document.getElementById("canvas").getContext("2d");
 let width=document.getElementById("canvas").width;
 let height=document.getElementById("canvas").height;
-let r=255;
-let g=255;
-let b=0;
 let a=255;
-
+let b=0;
+let r=255;
 let imageData=ctx.createImageData(width,height);//funcion creada por el contexto 
-
-for (let x = 0; x < height; x++) {
-   let  color=x/(height-1)*255;
-    for (let y = 0; y < width; y++) {
-        setPixel(imageData,y,x,color,color,color,a);//para el gradiente en vertical se pasan los parametros invertidos 
-        
-        
-        
-    }  
-}
+for (let x = 0; x < imageData.height; x++) {
+  for(let y=0;y<imageData.width;y++){
+    if(y<=imageData.width/2){
+      let color= y/(imageData.width/2-1)*255;
+      setPixel(imageData,y,x,color,color,b,a);
+    }else{
+      let color=(imageData.width/2)/y*255;
+      setPixel(imageData,y,x,r,color,b,a);
+    }
+    }
+  }
+ 
+    
 ctx.putImageData(imageData,0,0);
+
+
 function setPixel(imageData,x,y,r,g,b,a) {
   let  index=(x+y*imageData.width)*4;
-    imageData.data[index+0]=r;
-    imageData.data[index+1]=g;
-    imageData.data[index+2]=b;
-    imageData.data[index+3]=a;
-    
+  imageData.data[index+0]=r;
+  imageData.data[index+1]=g;
+  imageData.data[index+2]=b;
+  imageData.data[index+3]=a;
 }
--- tp1/pruebaGradiente.js
+++ tp1/pruebaGradiente.js
@@ -9,17 +9,17 @@ let a=255;
 
 let imageData=ctx.createImageData(width,height);//funcion creada por el contexto 
 
-for (let x = 0; x < height; x++) {
-   let  color=x/(height-1)*255;
-    for (let y = 0; y < width; y++) {
+for (let x = 0; x < imageData.height; x++) {
+   let  color=x/(imageData.width-1)*255;
+    for (let y = 0; y < imageData.width; y++) {
         setPixel(imageData,y,x,color,color,color,a);//para el gradiente en vertical se pasan los parametros invertidos 
-        
-        
-        
-    }  
-}
-ctx.putImageData(imageData,0,0);
+      }  
+    }
+ ctx.putImageData(imageData,0,0);
 function setPixel(imageData,x,y,r,g,b,a) {
+        
+        
+        
   let  index=(x+y*imageData.width)*4;
     imageData.data[index+0]=r;
     imageData.data[index+1]=g;
