8fb90a6451672e6d3eec675c00883dc0c6935384
belenenem <belenenemark@gmail.com>
2020-09-13 23:05:23 -0300

    los 4 filtros basicos

--- manejo archivo e imagenes/script.js
+++ manejo archivo e imagenes/script.js
@@ -48,12 +48,53 @@ input.onchange= e=>{
                 
             }*/
             //con esto lo paso a escala de grises
-            for(var i = 0; i < pixels.length; i += 4) {
+           /* for(var i = 0; i < pixels.length; i += 4) {
                 var grayscale= pixels[i]+pixels[i+1]+pixels[i+2]/3;
                 pixels[i]=grayscale;
                 pixels[i+1]=grayscale;
                 pixels[i+2]=grayscale;
-              }
+              }*/
+              //negativo
+             /* for(var i = 0; i < pixels.length; i ++) {
+                var r = pixels[ i * 4 ];
+                var g = pixels[ i * 4 + 1 ];
+                var b = pixels[ i * 4 + 2 ];
+         
+                pixels[ i * 4 ] = 255 - r;
+                pixels[ i * 4 + 1 ] = 255 - g;
+                pixels[ i * 4 + 2 ] = 255 - b;
+                   }*/
+                 //sepia  
+                /*for ( var i = 0; i < pixels.length; i++ ) {
+                    var r = pixels[ i * 4 ];
+                    var g = pixels[ i * 4 + 1 ];
+                    var b = pixels[ i * 4 + 2 ];
+             
+                    pixels[ i * 4 ] = 255 - r;
+                    pixels[ i * 4 + 1 ] = 255 - g;
+                    pixels[ i * 4 + 2 ] = 255 - b;
+             
+                    pixels[ i * 4 ] = ( r * .393 ) + ( g *.769 ) + ( b * .189 );
+                    pixels[ i * 4 + 1 ] = ( r * .349 ) + ( g *.686 ) + ( b * .168 );
+                    pixels[ i * 4 + 2 ] = ( r * .272 ) + ( g *.534 ) + ( b * .131 );
+                }*/
+                //brillo
+                /*
+                let contrast=150;
+                factor = ( 259 * ( contrast + 255 ) ) / ( 255 * ( 259 - contrast ) );
+ 
+                for ( var i = 0; i < pixels.length; i++ ) {
+                    var r = pixels[ i * 4 ];
+                    var g = pixels[ i * 4 + 1 ];
+                    var b = pixels[ i * 4 + 2 ];
+             
+                    pixels[ i * 4 ] = factor * ( r - 128 ) + 128;
+                    pixels[ i * 4 + 1 ] = factor * ( g - 128 ) + 128;
+                    pixels[ i * 4 + 2 ] = factor * ( b - 128 ) + 128;
+                }*/
+
+                
+            
             context.putImageData(imageData,0,0);
 
         }
