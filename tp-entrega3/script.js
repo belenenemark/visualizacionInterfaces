e8ec08db02251377e53aafaad5c773e1521f00ca
belenenem <belenenemark@gmail.com>
2020-10-19 23:13:22 -0300

    tp algunos puntos

--- tp-entrega3/script.js
+++
@@ -1,9 +0,0 @@
-fetch("https://belenenemark.github.io/visualizacionInterfaces/tp-entrega3/pruebapartial.html").then(
-  function(response){
-    response.text().then(
-  function(texto){
-    document.querySelector("#partial").innerHTML = texto;
-  }
-);
-  }
-);
